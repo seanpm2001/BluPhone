c105ff20d7ce23e9fac213cff577eb8726b21725
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 11:32:06 -0700

    HistorySettings.py
    The current version (version 1) of the history settings for the GUI Linux BluPhone client.

---
+++ Client/Linux/GUI/History/HistorySettings.py
@@ -0,0 +1,18 @@
+# Start of script
+# BluPhone Linux History settings
+print ("History\n")
+print ("Device history [Unable to open]")
+print ("Usage times [Unable to open]")
+print ("Clear history [unable to open]")
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 11:32 am)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 19
+'''
+# End of script
