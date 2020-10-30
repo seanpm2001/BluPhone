7ea2f034e3b2c772ca149034266978381aecdab6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 16:24:13 -0700

    SecuritySettings.p
    The current version (version 1) of the security settings page for the BluPhone Linux GUI client.

---
+++ Client/Linux/GUI/Security/SecuritySettings.py
@@ -0,0 +1,33 @@
+# Start of script
+# BluPhone Linux security settings
+print ("Security\n")
+print ("Password\n")
+print ("Set password [unable to open]")
+print ("Change password [unable to open]")
+print ("Remove password [unable to open]")
+print ("Test password [unable to open]")
+print ("Enable password [unable to open]")
+print ("Disable password [unable to open]")
+print ("\nTwo Factor Authentication (2FA)\n")
+print ("Enable 2FA [unable to open]")
+print ("Disable 2FA [unable to open]")
+print ("Set a 2FA question [unable to open]")
+print ("Remove 2FA info [unable to open]")
+print ("Test 2FA [unable to open]")
+print ("Change 2FA question [unable to open]")
+print ("\nDevice\n")
+print ("Reset device name [unable to open]")
+print ("Change device name [unable to open]")
+print ("Make device invisible once connected 1 time (will become visible once you disconnect) [unable to open]")
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 4:24 pm)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 34
+'''
+# End of script
