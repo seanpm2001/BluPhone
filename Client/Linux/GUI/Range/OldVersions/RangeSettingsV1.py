f80847cf2e69b8b6354bf4d97ba1e62ab9c66bb8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 11:36:45 -0700

    RangeSettingsV1.py
    An archive of the first version of the range settings for the BluPhone Linux GUI client.

---
+++ Client/Linux/GUI/Range/OldVersions/RangeSettingsV1.py
@@ -0,0 +1,24 @@
+# Start of script
+# BluPhone Linux range settings
+print ("Range\n")
+print ("Current distance [unable to open]")
+print ("Max distance [unable to open]")
+"""
+BlueTooth distance
+Class 1 - 100 meters (328 feet)
+Class 2 - 10 meters (33 feet)
+Class 3 - <10 meters (<33 feet)
+Source: https://www.scienceabc.com/innovation/what-is-the-range-of-bluetooth-and-how-can-it-be-extended.html
+"""
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 11:36 am)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 25
+'''
+# End of script
