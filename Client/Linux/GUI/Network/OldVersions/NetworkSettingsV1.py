0ae4c92f6d9941c533e81782e8b7a89bd54a01f8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 10:53:47 -0700

    NetworkSettingsV1.py
    An archive of the first version of the network settings file for the BluPhone Linux client.

---
+++ Client/Linux/GUI/Network/OldVersions/NetworkSettingsV1.py
@@ -0,0 +1,41 @@
+# Start of script
+# BluPhone Linux Network settings
+print ("Network")
+print ("\nGeneral")
+print ("BlueTooth [Unable to open]")
+print ("\nBlueTooth")
+print ("Current devices [Unable to open]")
+print ("Currently has access [Unable to open]")
+print ("BlueTooth [ON/OFF] [Unable to open]")
+print ("Wi-Fi [Unable to open]")
+print ("\nCurrent devices [Unable to open]")
+print ("Currently has access [Unable to open]")
+print ("Wi-Fi [ON/OFF] [Unable to open]")
+print ("\nCellular 2G")
+print ("\nCurrent devices [Unable to open]")
+print ("Currently has access [Unable to open]")
+print ("2G [ON/OFF] [Unable to open]")
+print ("\nCellular 3G")
+print ("\nCurrent devices [Unable to open]")
+print ("Currently has access [Unable to open]")
+print ("3G [ON/OFF] [Unable to open]")
+print ("\nCellular 4G/")
+print ("\nCurrent devices [Unable to open]")
+print ("Currently has access [Unable to open]")
+print ("4G [ON/OFF] [Unable to open]")
+print ("\nCellular 5G")
+print ("\nCurrent devices [Unable to open]")
+print ("Currently has access [Unable to open]")
+print ("5G [ON/OFF] [Unable to open]\n")
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 10:53 am)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 42
+'''
+# End of script
