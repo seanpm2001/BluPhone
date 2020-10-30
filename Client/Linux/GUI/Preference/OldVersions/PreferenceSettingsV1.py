dc9219c4eb3ca7c9a4e070cf741e290cc50ff496
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 11:45:58 -0700

    PreferenceSettingsV1.py
    An archive of the first version of the preference settings for the BluPhone GUI Linux client.

---
+++ Client/Linux/GUI/Preference/OldVersions/PreferenceSettingsV1.py
@@ -0,0 +1,17 @@
+# Start of script
+# BluPhone Linux Preference settings
+print ("Preference\n")
+print ("Headphones [unable to open]")
+print ("Earphones/Earbuds [unable to open]")
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 11:45 am)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 18
+'''
+# End of script
