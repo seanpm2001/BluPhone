440ed463d35ec28cd2766cde2a3cb581af003af2
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 11:00:05 -0700

    SoundSettings.py
    The current version (version 1) of the sound settings page for the BluPhone Linux client.

---
+++ Client/Linux/GUI/Sound/SoundSettings.py
@@ -0,0 +1,45 @@
+# Start of script
+# BluPhone Linux sound settings
+print ("Sound")
+print ("\nBlocked sound\n")
+print ("Repeat blocked sound [Unable to open]")
+print ("Blocked sound by bit [Unable to open]")
+print ("Blocked sound by kilobit [Unable to open]")
+print ("Blocked sound by megabit [Unable to open]")
+print ("Blocked sound by gigabit [Unable to open]")
+print ("Blocked sound by terabit [Unable to open]")
+print ("\nSound test\n")
+print ("Front left [Unable to open]")
+print ("Front right [Unable to open]")
+print ("Back left [Unable to open]")
+print ("Back right [Unable to open]")
+print ("\nQuality\n")
+print ("Current quality [Unable to open]")
+print ("MIDI mode [Unable to open]")
+print ("4 bit mode [Unable to open]")
+print ("6 bit mode [Unable to open]")
+print ("8 bit mode [Unable to open]")
+print ("12 bit mode [Unable to open]")
+print ("16 bit mode [Unable to open]")
+print ("18 bit mode [Unable to open]")
+print ("20 bit node [Unable to open]")
+print ("24 bit mode [Unable to open]")
+print ("28 bit mode [Unable to open]")
+print ("32 bit mode [Unable to open]")
+print ("36 bit mode [Unable to open]")
+print ("42 bit mode [Unable to open]")
+print ("48 bit mode [Unable to open]")
+print ("54 bit mode [Unable to open]")
+print ("64 bit mode [Unable to open]")
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 11:00 am)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 46
+'''
+# End of script
