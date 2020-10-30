b05e50f896bdb502f879d537d0fde97393b6028c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 11:50:39 -0700

    WarningSettingsV1.py
    An archive of the first version of the warning settings for the BluPhone LInux GUI client.

---
+++ Client/Linux/GUI/Warnings/OldVersions/WarningSettingsV1.py
@@ -0,0 +1,25 @@
+# Start of script
+# BluPhone Linux Warning settings
+print ("Warning types\n")
+print ("Test all [unable to open]")
+print ("Test: low battery [unable to open]")
+print ("Test: disconnected [unable to open]")
+print ("Test: connected [unable to open]")
+print ("Test: pairing [unable to open]")
+print ("Test: out of range [unable to open]")
+print ("Test: mute [unable to open]")
+print ("Test: unmute [unable to open]")
+print ("Test: power on [unable to open]")
+print ("Test: power off [unable to open]")
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 11:50 am)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 26
+'''
+# End of script
