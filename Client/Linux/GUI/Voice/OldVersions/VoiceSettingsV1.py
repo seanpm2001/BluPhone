8a36970867ee2d8ebbe66c65fd6cfe9e8132f202
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-30 11:48:59 -0700

    VoiceSettingsV1.py
    An archive of the first version of the voice settings page for the BluPhone Linux GUI client.

---
+++ Client/Linux/GUI/Voice/OldVersions/VoiceSettingsV1.py
@@ -0,0 +1,18 @@
+# Start of script
+# BluPhone Linux Voice settings
+print ("Voice\n")
+print ("Change automated voice\n")
+print ("Voice1 [unable to open]")
+print ("Voice2 [unable to open]")
+noMore = input("Press [ENTER] key to exit current tab")
+print ("This window should be closed now")
+"""
+I don't know enough about Python yet to make a functional GUI or to make these options openable
+"""
+'''
+File info
+File version: 1 (Friday, October 30th 2020 at 11:48 am)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 19
+'''
+# End of script
