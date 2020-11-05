352818b77e50df3479df72d7dfdc61edbcdad74e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-04 17:49:58 -0800

    ConnectV1.py
    An archive of the first version of the BluPhoneOS Connection script.

---
+++ BluPhoneOS/OldVersions/ConnectV1.py
@@ -0,0 +1,12 @@
+# Start of script
+print ("Connecting to BluPhoneOS on <deviceID::0x00000000>")
+print ("Error, this functionality is not yet available")
+noMore = input("Press [EMTER] key to quit")
+print ("The program should now be closed. If the window is still open, try pressing the close button. If this doesn't work, try ending the process with a process killer/task manager")
+# End of script
+"""
+File info
+File version: 1 (Wednesday, November 4th 2020 at 5:49 pm)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler lines): 13
+"""
